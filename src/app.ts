356a3fd44bf7638075c870dc1f61b3352174b61d
Oleh Lozovyk <o.lozovik@gmail.com>
2022-07-30 18:35:33 +0300

    logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from 'express';
 import { userRouter } from './users/users.js';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service.js';
 
 export class App {
   app: Express;
   port: number;
   server: Server;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -20,6 +23,6 @@ export class App {
     this.useRoutes();
 
     this.server = this.app.listen(this.port);
-    console.log(`Server run on http://localhost:${this.port}/`);
+    this.logger.log(`Server run on http://localhost:${this.port}/`);
   }
 }
